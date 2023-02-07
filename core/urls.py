885b31a3b89a58dd470120f3a48830512e5fe639
PaulMaiyaniSerian <paulmaiyanideveloper@gmail.com>
2023-02-07 13:22:52 +0300

    updates

--- core/urls.py
+++ core/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     # cart and shopping_session
     path('shopping_session/create',views.ShoppingSessionCreateView.as_view(), name="shopping_session_create" ),
     path('shopping_session/cart_item/add',views.AddToCartView.as_view(), name="add_to_cart" ),
+    path('shopping_session/cart_item/update',views.UpdateCartItem.as_view(), name="update_cart" ),
+    path('shopping_session/cart_items',views.GetCartItemsView.as_view(), name="cartItems" ),
+    path('shopping_session/shopping_session/total',views.GetShoppingSessionTotal.as_view(), name="shopping_session_total" ),
+
+
 
 
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 # serializer imports
@@ -138,7 +139,38 @@ class ShoppingSessionCreateView(generics.GenericAPIView):
                     return Response(status=status.HTTP_200_OK)
 
 
+def formatCartItems(request, cart_items):
+    result = []
+    for cart_item in cart_items:
+        data = {
+            "id" : cart_item.id,
+            "shopping_session_id": cart_item.shopping_session.id,
+            "product_id": cart_item.product.id,
+            "product_name": cart_item.product.name,
+            "product_price": cart_item.product.price,
+            "quantity": cart_item.quantity,
+            "total": cart_item.total,
+            "desktop_img": request.build_absolute_uri(cart_item.product.desktop_img.url)
+        }
 
+        result.append(data)
+    
+    return result
+
+def formatCartItem(request, cart_item):
+    
+    data = {
+        "id" : cart_item.id,
+        "shopping_session_id": cart_item.shopping_session.id,
+        "product_id": cart_item.product.id,
+        "product_name": cart_item.product.name,
+        "product_price": cart_item.product.price,
+        "quantity": cart_item.quantity,
+        "total": cart_item.total,
+        "desktop_img": request.build_absolute_uri(cart_item.product.desktop_img.url)
+    }
+
+    return data
             
             
 # add to cart session
@@ -166,36 +198,263 @@ class AddToCartView(generics.GenericAPIView):
             # logged in users cart
             # query Shopping session of the user
             shopping_session = ShoppingSession.objects.get(user=user)
-          
 
-            # create cartItem
-            cartItem = CartItem.objects.create(
-                shopping_session=shopping_session,
-                product = product_id,
-                quantity = qty,
-                total = total
-            )
+            # chech if product exists in shopping session if it exists  add 1 to qty and calculate the total
+            try:
+                cartItem = CartItem.objects.get(product=product, shopping_session=shopping_session)
+                cartItem.quantity += 1
+                cartItem.total = product.price * cartItem.quantity
+                cartItem.save()
+                print("cart item updated")
 
-            serializer = CartItemSerializer(cartItem)
+                # serializer = CartItemSerializer(cartItem)
+                cart_item = formatCartItem(request, cartItem)
 
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+                return Response(data=cart_item, status=status.HTTP_200_OK)
+
+
+            except CartItem.DoesNotExist:
+                print("cart item does not exist, creating")
+                # create cartItem
+                cartItem = CartItem.objects.create(
+                    shopping_session=shopping_session,
+                    product = product,
+                    quantity = qty,
+                    total = total
+                )
+
+                cart_item = formatCartItem(request, cartItem)
+
+
+                return Response(data=cart_item, status=status.HTTP_201_CREATED)
         
         else:
             # means not logged in so use device_id
             # query shopping session
             shopping_session = ShoppingSession.objects.get(device_id=device_id)
 
-            # create cart item based on device_id
-            cartItem = CartItem.objects.create(
-                shopping_session=shopping_session,
-                product = product_id,
-                quantity = qty,
-                total = total
-            )
 
-            serializer = CartItemSerializer(cartItem)
+            # chech if product exists in shopping session if it exists  add 1 to qty and calculate the total
+            try:
+                cartItem = CartItem.objects.get(product=product, shopping_session=shopping_session)
+                cartItem.quantity += 1
+                # cartItem.total = product.price * cartItem.quantity
+                cartItem.save()
+                print("cart item updated")
 
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+                # serializer = CartItemSerializer(cartItem)
+                cart_item = formatCartItem(request, cartItem)
+
+
+                return Response(data=cart_item, status=status.HTTP_200_OK)
+
+            except CartItem.DoesNotExist:
+                # create cart item based on device_id
+                cartItem = CartItem.objects.create(
+                    shopping_session=shopping_session,
+                    product = product,
+                    quantity = qty,
+                    total = total
+                )
+
+                # serializer = CartItemSerializer(cartItem)
+                cart_item = formatCartItem(request, cartItem)
+
+
+                return Response(data=cart_item, status=status.HTTP_201_CREATED)
 
 
 
+
+
+class GetCartItemsView(generics.GenericAPIView):
+    serializer_class = CartItemSerializer
+
+    def get(self, request):
+        user = request.user if request.user.is_authenticated else None
+
+        device_id = request.query_params.get('device_id')
+
+        if user:
+            # query the users session then cart items
+            try:
+                shopping_session = ShoppingSession.objects.get(user=user)
+            except ShoppingSession.DoesNotExist:
+                shopping_session = None
+            
+            if shopping_session:
+                cart_items = CartItem.objects.filter(shopping_session=shopping_session)
+
+                # serializer = CartItemSerializer(cart_items, many=True)
+
+                result = formatCartItems(request, cart_items)
+
+                return Response(result, status=status.HTTP_200_OK)
+        
+        else:
+            # query session for device id
+            try:
+                shopping_session = ShoppingSession.objects.get(device_id=device_id)
+            except ShoppingSession.DoesNotExist:
+                shopping_session = None
+            
+            if shopping_session:
+                cart_items = CartItem.objects.filter(shopping_session=shopping_session)
+
+                # serializer = CartItemSerializer(cart_items, many=True)
+                result = formatCartItems(request, cart_items)
+
+                return Response(result, status=status.HTTP_200_OK)
+
+        
+        return Response({
+            "error": "device_id or logged in user attribute required"
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+def calculateCartItemsTotal(cart_items):
+    result = {
+        "grand_total" : 0
+    }
+    for cart_item in cart_items:
+        result["grand_total"] += cart_item.total
+    
+    return result
+
+
+
+class GetShoppingSessionTotal(generics.GenericAPIView):
+    serializer_class = CartItemSerializer
+
+    def get(self, request):
+        user = request.user if request.user.is_authenticated else None
+
+        device_id = request.query_params.get('device_id')
+
+        if user:
+            # query the users session then cart items
+            try:
+                shopping_session = ShoppingSession.objects.get(user=user)
+            except ShoppingSession.DoesNotExist:
+                shopping_session = None
+            
+            if shopping_session:
+                cart_items = CartItem.objects.filter(shopping_session=shopping_session)
+
+                # serializer = CartItemSerializer(cart_items, many=True)
+
+                result = calculateCartItemsTotal( cart_items)
+
+                return Response(result, status=status.HTTP_200_OK)
+        
+        else:
+            # query session for device id
+            try:
+                shopping_session = ShoppingSession.objects.get(device_id=device_id)
+            except ShoppingSession.DoesNotExist:
+                shopping_session = None
+            
+            if shopping_session:
+                cart_items = CartItem.objects.filter(shopping_session=shopping_session)
+
+                # serializer = CartItemSerializer(cart_items, many=True)
+                result = calculateCartItemsTotal(cart_items)
+
+                return Response(result, status=status.HTTP_200_OK)
+
+        
+        return Response({
+            "error": "device_id or logged in user attribute required"
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# add to cart session
+class UpdateCartItem(generics.GenericAPIView):
+    serializer_class = CartItemSerializer
+
+    def put(self, request):
+        data = request.data
+
+        product_id = data.get('product_id')
+
+        device_id = data.get('device_id')
+
+        qty = data.get("qty")
+
+        try:
+            qty = int(qty)
+        except:
+            qty = 1
+
+        user = request.user if request.user.is_authenticated else None
+
+        product = Product.objects.get(id=product_id)
+
+        # calculate Total
+        total = product.price * int(qty)
+
+
+        if user:
+            # logged in users cart
+            # query Shopping session of the user
+            shopping_session = ShoppingSession.objects.get(user=user)
+
+            # chech if product exists in shopping session if it exists  add 1 to qty and calculate the total
+            try:
+                cartItem = CartItem.objects.get(product=product, shopping_session=shopping_session)
+                cartItem.quantity = qty
+                cartItem.total = product.price * cartItem.quantity
+                cartItem.save()
+                print("cart item updated")
+
+                serializer = CartItemSerializer(cartItem)
+
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+            except CartItem.DoesNotExist:
+                print("cart item does not exist, creating")
+                # create cartItem
+                cartItem = CartItem.objects.create(
+                    shopping_session=shopping_session,
+                    product = product,
+                    quantity = qty,
+                    total = total
+                )
+
+                serializer = CartItemSerializer(cartItem)
+
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        
+        else:
+            # means not logged in so use device_id
+            # query shopping session
+            shopping_session = ShoppingSession.objects.get(device_id=device_id)
+
+
+            # chech if product exists in shopping session if it exists  add 1 to qty and calculate the total
+            try:
+                cartItem = CartItem.objects.get(product=product, shopping_session=shopping_session)
+                cartItem.quantity = qty
+                # cartItem.total = product.price * cartItem.quantity
+                cartItem.save()
+                print("cart item updated")
+
+                serializer = CartItemSerializer(cartItem)
+
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+            except CartItem.DoesNotExist:
+                # create cart item based on device_id
+                cartItem = CartItem.objects.create(
+                    shopping_session=shopping_session,
+                    product = product,
+                    quantity = qty,
+                    total = total
+                )
+
+                serializer = CartItemSerializer(cartItem)
+
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
